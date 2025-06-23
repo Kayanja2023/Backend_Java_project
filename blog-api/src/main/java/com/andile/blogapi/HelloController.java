b751236b71cd149e448949ea4134926ce1ef84ab
andile.lwanga <andile.lwanga@clickatell.com>
2025-06-23 12:37:41 +0200

    pom.xml updated and HelloController.java tested

---
+++ blog-api/src/main/java/com/andile/blogapi/HelloController.java
@@ -0,0 +1,13 @@
+package com.andile.blogapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "🚀 Hello, Spring Boot is running!";
+    }
+}
